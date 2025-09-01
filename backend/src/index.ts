aafba5ec41a88e880390474676f3826dabe5d3ac
Marcel Frey <marcelfrey29@mailbox.org>
2025-09-01 19:24:22 +0200

    feat(backend): add endpoint to get a `Recipe` by ID
    relates to #6

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -113,6 +113,72 @@ app.post("/recipes", async (c) => {
     return c.json(storedRecipe, 201);
 });
 
+app.get("/recipes/:id", async (c) => {
+    // Get Recipe ID from Path Parameter
+    let id: number;
+    try {
+        id = parseInt(c.req.param("id"));
+    } catch (e) {
+        logger.warn({}, "The provided ID is not a number.");
+        return c.json({}, 400);
+    }
+
+    // Get the Recipe by ID from the Databse
+    const recipe = await (await dbService.getDatabaseManager())
+        .getRepository(Recipe)
+        .findOneBy({ id });
+
+    // Return 404 Not Found if there is no Recipe with the given ID in the Database.
+    if (recipe === null) {
+        logger.warn({ id }, "No Recipe for the ID found.");
+        return c.json({}, 404);
+    }
+
+    /**
+     * BUG: https://github.com/marcelfrey29/IU-DOCC-Project-Secure-Software-Implementation/issues/18
+     *
+     * # Description
+     *
+     * Private recipes are exposed to unauthorized users. This includes both anonymous users, and logged-in users
+     * that don't own the recipe.
+     * The ID can also be easily guessed as it's an integer counter ( `1`, `2`, `3`, ...).
+     *
+     * CWE-566 (Authorization Bypass Through User-Controlled SQL Primary Key) contributes to this issue but is not the
+     * root cause. In our case Recipes are requested by their ID which is sent to the backend in a path parameter. This
+     * ID is directly passed to the `findOneBy()` method, so the Primary Key is controlled by the user.
+     * CWE 566 is part of CWE-639 (Authorization Bypass Through User-Controlled Key) but as already mentioned that's not
+     * the root cause of the vulnerability.
+     *
+     * In our case it's also possible to guess the IDs of other recipies as we're using a number as Primary Key which is
+     * increased for every new record. A threat actor can therefore easily scrape recipies by increasing a counter and
+     * perform the request. To prevent guessing of IDs, a UUID should be used as identifier (and primary key).
+     *
+     * The root cause of this vulnerability is CWE-862 (Missing Authorization) because ther is no check that compares the
+     * `ownerUserId` of the `Recipe` to the `sub` (User ID) of the requesting user. All these CWEs are part of OWASP
+     * Top 10 A2021:01 (Broken Access Control).
+     *
+     * # Impact
+     *
+     * Sensitive data are exposed which violates the confidentiality of the data.
+     *
+     * # Background
+     *
+     * https://owasp.org/Top10/A01_2021-Broken_Access_Control/
+     * https://cwe.mitre.org/data/definitions/566.html
+     * https://cwe.mitre.org/data/definitions/639.html
+     * https://cwe.mitre.org/data/definitions/862.html
+     *
+     * # Remediation
+     *
+     * Add an explicit ownership check for private recipes by comparing the `ownerUserId` of the `Recipe` entity
+     * to the `sub` from the access token of the requesting user.
+     * Consider using UUIDs as identifier that can't be easily guessed.
+     */
+
+    // Return the Recipe to the Client
+    return c.json(recipe, 200);
+});
+
 // Run Server
 serve(
     {
--- docs/03-Flaws.md
+++ docs/03-Flaws.md
@@ -4,3 +4,4 @@
 | --- | ------------ | ------------- | ---------- | ---- |
 | 1   | [Auth-Service Access Token in Terraform Source Code (#14)](https://github.com/marcelfrey29/IU-DOCC-Project-Secure-Software-Implementation/issues/14)  | `terraform/main.tf`  | Hard-coded Credentials | CWE-259, CWE-789 |
 | 2   | [PostgreSQL Password in Source Code (#15)](https://github.com/marcelfrey29/IU-DOCC-Project-Secure-Software-Implementation/issues/15)  | `backend/src/config/database.ts`  | Hard-coded Credentials | CWE-259, CWE-789 |
+| 3   | [Public exposure of private Recipes (Broken Access Control) (#18)](https://github.com/marcelfrey29/IU-DOCC-Project-Secure-Software-Implementation/issues/18) | `backend/src/index.ts` | Broken Access Control | CWE-566, CWE-639, CWE-862 |
