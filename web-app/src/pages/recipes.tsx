9084ec3db3d8d0f1289c0d754e705f90fda483d2
Marcel Frey <marcelfrey29@mailbox.org>
2025-09-03 21:03:05 +0200

    feat(web-app): add recipe overview page
    closes #5

--- web-app/src/pages/recipes.tsx
+++ web-app/src/pages/recipes.tsx
@@ -1,12 +1,31 @@
 import { BootstrapIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import { Recipe, RecipesService } from "@/service/recipe-service";
 import { Button } from "@heroui/button";
+import { Card, CardBody } from "@heroui/card";
+import { Chip } from "@heroui/chip";
+import { Image } from "@heroui/image";
+import { useEffect, useState } from "react";
 import { useAuth } from "react-oidc-context";
 import { useNavigate } from "react-router-dom";
+
+const recipeService = new RecipesService();
 
 export default function RecipesPage() {
     const routeTo = useNavigate();
     const auth = useAuth();
+
+    const [recipes, setRecipes] = useState([] as Recipe[]);
+    const getRecipe = async () => {
+        setRecipes(
+            await recipeService.getRecipes({
+                accessToken: auth.user?.access_token,
+            }),
+        );
+    };
+    useEffect(() => {
+        getRecipe();
+    }, []);
 
     return (
         <DefaultLayout>
@@ -53,6 +72,120 @@ export default function RecipesPage() {
                                 </div>
                             </>
                         )}
+                    </div>
+                </div>
+                <div className="mt-8">
+                    <div>
+                        {recipes.map((recipe) => {
+                            return (
+                                <>
+                                    <Card className="mt-5" shadow="md">
+                                        <CardBody>
+                                            <div className="flex">
+                                                <div className="flex mr-4">
+                                                    <div className="mt-auto mb-auto">
+                                                        <Image
+                                                            height={150}
+                                                            src="/public/logo.png"
+                                                        />
+                                                    </div>
+                                                </div>
+
+                                                <div className="flex">
+                                                    <div className="mt-auto mb-auto">
+                                                        {recipe.isPrivate ? (
+                                                            <>
+                                                                <Chip
+                                                                    variant="bordered"
+                                                                    color="danger"
+                                                                >
+                                                                    <BootstrapIcon name="lock-fill" />{" "}
+                                                                    Private
+                                                                    Recipe
+                                                                </Chip>
+                                                            </>
+                                                        ) : (
+                                                            <></>
+                                                        )}
+                                                        <h4 className="font-bold text-xl">
+                                                            {recipe.title}
+                                                        </h4>
+                                                        <p className="text-large">
+                                                            {recipe.description}
+                                                        </p>
+                                                        <p className="text-small text-foreground/80 mt-3">
+                                                            {
+                                                                recipe
+                                                                    .ingredients
+                                                                    .length
+                                                            }{" "}
+                                                            Ingredients -{" "}
+                                                            {
+                                                                recipe.steps
+                                                                    .length
+                                                            }{" "}
+                                                            Steps
+                                                        </p>
+                                                        <p className="text-small text-foreground/80 mt-1">
+                                                            {" "}
+                                                            By{" "}
+                                                            {recipe.ownerUserId}
+                                                        </p>
+                                                    </div>
+                                                </div>
+
+                                                <div className="ml-auto flex">
+                                                    <div className="mt-auto mb-auto">
+                                                        <div>
+                                                            <Button
+                                                                fullWidth
+                                                                color="primary"
+                                                                onClick={() =>
+                                                                    routeTo(
+                                                                        `/recipes/${recipe.id}`,
+                                                                    )
+                                                                }
+                                                            >
+                                                                View Recipe{" "}
+                                                                <BootstrapIcon name="chevron-right" />
+                                                            </Button>
+                                                        </div>
+                                                        {recipe.ownerUserId ===
+                                                        auth.user?.profile
+                                                            .sub ? (
+                                                            <>
+                                                                <div className="mt-2 hidden">
+                                                                    <Button
+                                                                        fullWidth
+                                                                        color="warning"
+                                                                    >
+                                                                        Edit
+                                                                        Recipe{" "}
+                                                                        <BootstrapIcon name="pencil-fill" />
+                                                                    </Button>
+                                                                </div>
+                                                                <div className="mt-2 hidden">
+                                                                    <Button
+                                                                        fullWidth
+                                                                        color="danger"
+                                                                    >
+                                                                        Delete
+                                                                        Recipe{" "}
+                                                                        <BootstrapIcon name="trash-fill" />
+                                                                    </Button>
+                                                                </div>
+                                                            </>
+                                                        ) : (
+                                                            <></>
+                                                        )}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </CardBody>
+                                    </Card>
+                                </>
+                            );
+                        })}
                     </div>
                 </div>
             </section>
